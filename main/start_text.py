d5987439a573b9397b0511c9e4db0e3589a8b711
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-23 02:12:44 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -5,11 +5,11 @@ from config import ADMIN
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
-    txt="𝚃𝙷𝙸𝚂 𝙸𝚂 𝚒𝚜 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝙻 𝚄𝚂𝙴 𝙱𝙾𝚃 🙏,\n𝙳𝙾 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝙰𝙽𝚈 𝙾𝚃𝙷𝙴𝚁 𝙱𝙾𝚃𝚂?\n👇 𝙲𝙻𝙸𝙲𝙺 𝚃𝙷𝙴 𝙱𝚄𝚃𝚃𝙾𝙽 𝙱𝙴𝙻𝙾𝚆 𝙰𝙽𝙳 𝙹𝙾𝙸𝙽 𝙱𝙾𝚃𝙷 𝙲𝙷𝙰𝙽𝙽𝙴𝙻."
+    text="**Please Join My Both Updates Channel to use this Bot!**"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 𝙾𝚃𝙷𝙴𝚁 𝙱𝙾𝚃𝚂 🤖", url="https://t.me/iPepkornBots")
+        InlineKeyboardButton("🔥 Join Updates Channel 🔥", url="https://youtube.com/@InvisibleYTV")
         ],[
-        InlineKeyboardButton("🎉 𝙱𝙴𝚂𝚃 𝙳𝙴𝙰𝙻𝚂 🎉", url="https://t.me/TrueDealsMaster")
+        InlineKeyboardButton("🎉 Subscribe 🎉", url="https://t.me/+VnG269AYxSM3OGFl")
     ]])
     if msg.from_user.id != ADMIN:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
@@ -18,16 +18,11 @@ async def start_cmd(bot, msg):
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"👋Hii {msg.from_user.mention}\n<b>𝙼𝚈𝚂𝙴𝙻𝙵 @SimpleRenameBot \n\n𝙸 𝙰𝙼 𝚂𝙸𝙼𝙿𝙻𝙴 𝚁𝙴𝙽𝙰𝙼𝙴 𝙱𝙾𝚃 𝚆𝙸𝚃𝙷 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝙻 𝚄𝚂𝙰𝙶𝙴,\n𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 𝙸𝚂 𝙼𝙰𝙳𝙴 𝙱𝚈 <b><a href=https://t.me/iPepkornBots>𝚒𝙿𝚊𝚙𝚔𝚘𝚛𝚗𝙱𝚘𝚝𝚜</a></b>"                                     
+    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage."
     button= [[
-        InlineKeyboardButton("📢 𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢", url="https://t.me/iPapkon")
-        ],[
-        InlineKeyboardButton("🤖 𝙱𝙾𝚃𝚂 🤖", url="https://t.me/iPepkornBots"),
-        InlineKeyboardButton("🎉 𝙳𝙴𝙰𝙻𝚂 🎉", url="https://t.me/TrueDealsMaster")
-        ],[
-        InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿 ℹ️", callback_data="help"),
-        InlineKeyboardButton("💫 𝙰𝙱𝙾𝚄𝚃 💫", callback_data="about") 
-    ]]  
+        InlineKeyboardButton("ℹ️ Help", callback_data="help"),
+        InlineKeyboardButton("📡 About", callback_data="about") 
+    ]]   
     if cb:
         await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
     else:
