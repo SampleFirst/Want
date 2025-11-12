7e8a846d87d9af2a600d609d6c8976f240c8f7cc
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-11-12 23:59:02 +0530

    Update BOT_TOKEN and ADMIN values in config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23906038"))
 API_HASH = environ.get("API_HASH", "dff1eb42fad7971f16da662a99c0f376")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6280404711:AAGa4YnULAUH2obc6sloa1yes3F7TI095gg")
-ADMIN = int(environ.get("ADMIN", "5217294686"))          
+BOT_TOKEN = environ.get("BOT_TOKEN", "7065109515:AAHEM2EJIU01PtSkzk4oTxffiATNBfc2SrY")
+ADMIN = int(environ.get("ADMIN", "5433924139"))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
