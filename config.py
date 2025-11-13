e449a21ec4816b5df5e96df4ae7f1b87a4b711cc
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-11-13 20:14:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23906038"))
 API_HASH = environ.get("API_HASH", "dff1eb42fad7971f16da662a99c0f376")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7065109515:AAHEM2EJIU01PtSkzk4oTxffiATNBfc2SrY")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7065109515:AAHFa33484uH1v9hBAmsj9MgPQrtK9KqKvM")
 ADMIN = int(environ.get("ADMIN", "5433924139"))          
 CAPTION = environ.get("CAPTION", "")
 
